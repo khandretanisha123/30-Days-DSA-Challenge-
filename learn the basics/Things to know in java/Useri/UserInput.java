ad270f7dca8918fa6f1c602d1dc3d69cb842ce5d
Tanisha Khandre <khandretanisha@gmail.com>
2026-01-12 18:49:34 +0530

    complete

--- learn the basics/Things to know in java/Useri/UserInput.java
+++ learn the basics/Things to know in java/Useri/UserInput.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class UserInput{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int a = sc.nextInt();
+        int  a = sc.nextInt();
         System.out.println(a);
     }
 }

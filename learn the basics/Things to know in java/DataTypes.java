c7ce4f2fa817749090140c6ebb65c8d27fb10ca9
Tanisha Khandre <khandretanisha@gmail.com>
2026-01-12 19:15:09 +0530

    complete

---
+++ learn the basics/Things to know in java/DataTypes.java
@@ -0,0 +1,22 @@
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //primitive data types
+        byte a = 127;
+        System.out.println(a); //store 1 bytes
+        short b = 32000;
+        System.out.println(b); //store 2 bytes
+        char  c = 'A';
+        System.out.println(c); // store 2 bytes
+        int d = 100000;
+        System.out.println(d); //store 4 bytes
+        float e = 3.14f;
+        System.out.println(e); //store 4 bytes
+        long  f = 10000000;
+        System.out.println(f); //store 8 bytes
+        double g = 3.1456738;
+        System.out.println(g); //store 8 bytes
+        // boolean store 1 bit i.e True or false
+    }
+
+}
